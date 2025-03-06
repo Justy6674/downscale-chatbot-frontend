9242788ead990e97dcfbed02c77d4dff18770af5
Justin C Black <jb-downscale@Mac.hub>
2025-03-06 11:17:16 +1000

    Explicitly separated Tailwind from Vite config (final fix)

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',  // ✅ Ensures assets are referenced correctly
   plugins: [react()],
   build: {
-    outDir: "dist"  // ✅ Ensures the correct output directory
-  }
+    outDir: 'dist',
+  },
 });
+
